a364dc04ee59b09751740b4b6e7aaec13eb2152b
mmor0728 <60114465+mmor0728@users.noreply.github.com>
2024-03-27 15:49:18 -0500

    Update App.tsx

--- personalwebsite/src/App.tsx
+++ personalwebsite/src/App.tsx
@@ -1,9 +1,5 @@
 import './App.css'
-//import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';;
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import portrait from "./assets/MMORPORTRAIT.jpg";
 function App() {
